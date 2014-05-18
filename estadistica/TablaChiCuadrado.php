bc279c50be04d8af6e8f4a0e80ea1d97a326308e
Ronny Polloqueri <ronny.polloqueri@gmail.com>
2014-05-18 17:07:46 -0500

    Terminada clase TablaChiCuadrado, ingresar valores de prueba al final del archivo.

--- estadistica/TablaChiCuadrado.php
+++ estadistica/TablaChiCuadrado.php
@@ -1,14 +1,37 @@
 <?php
-$fila = 1;
-if (($gestor = fopen("tablas_csv/chicuadrado.csv", "r")) !== FALSE) {
-    while (($datos = fgetcsv($gestor, 1000, ",")) !== FALSE) {
-            $numero = count($datos);
-            echo "<p> $numero de campos en la línea $fila: <br /></p>\n";
-            $fila++;
-            for ($c=0; $c < $numero; $c++) {
-                        echo $datos[$c] . "<br />\n";
-                    }
-               }
-       fclose($gestor);
+class TablaChiCuadrado{
+    private static $matriz = array();
+    private static $nivel_de_significancia = array();
+
+    /*
+     * Guardará los valores como números no como string
+     */
+    private static function start(){
+        $fila = 1;
+        if (($gestor = fopen("tablas_csv/chicuadrado.csv", "r")) !== FALSE) {
+        while (($datos = fgetcsv($gestor, 1000, ",")) !== FALSE) {
+                if($fila == 1){
+                    self::$nivel_de_significancia = array_slice($datos,1);
+                    $fila++;
+                } else {
+                    self::$matriz[$datos[0]] = array_slice($datos,1);
+                }
+            }
+               fclose($gestor);
+        }
+    }
+
+    public static function get($alfa, $grados){
+        self::start();
+        $indice = array_search($alfa, self::$nivel_de_significancia);
+        return self::$matriz[$grados][$indice];
+    }
 }
+/*
+ * [Pruebas]
+ * Ingresar TabaChicuadrado(nivel_de_significancia, grados_de_libertad);
+ */
+echo TablaChiCuadrado::get(0.005,2).PHP_EOL;
+echo TablaChiCuadrado::get(0.005,2).PHP_EOL;
+echo TablaChiCuadrado::get(0.005,2).PHP_EOL;
 ?>
